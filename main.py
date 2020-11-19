572b8e243ee77f6647a9458af1ed6a9bc6b26345
arsydewantara <74332636+arsydewantara@users.noreply.github.com>
2020-11-19 14:17:25 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#Lokasi Wisata Balikpapan
+balikpapan_alam = ("\n1.Pantai Kemala \n2.Danau Cermin")
+balikpapan_kuliner = ("\n1.Ruko Bandar \n2.Kepiting Dandito \n3.Ocean's Resto Balikpapan")
+balikpapan_keluarga = ("\n1.Aquaboom Waterpark \n2.Taman Bekapai \n3.Caribbean Islan Waterpark")
+balikpapan_belanja = ("\n1.Pasar Inpres Kebun Sayur \n2.Beruang Madu \n3.Kawasan Makanan Ringan Stal Kuda")
+balikpapan_sejarah = ("\n1.Tugu Australia \n2.Museum Mulawarman \n3.Sumur Minyak Mathilda B-1(1896-1903)")
+balikpapan_religi = ("\n1.Masjid Jami'al ULA \n2.Vihara Dharmasatya")
+
+#Lokasi Wisata Bontang
+bontang_alam = ("\n1.Pulau Beras Basah \n2.Bontang Mangrove")
+bontang_kuliner = ("\n1.Cafe Singapore \n2.Wiwako \n3.Jimbaran Cafe")
+bontang_keluarga = ("\n1.Kenari Waterpark \n2.Taman Tanjung Laut \n3.Taman Cibodas")
+bontang_belanja = ("\n1.Bontang Plaza \n2.Pujasera Tahap 1 BTN-PKT \n3.Pasar Malam Berbas")
+bontang_religi = ("\n1.Masjid Tua Al Wahhab")
+
+#Lokasi Wisata Kutai Barat
+kutaibarat_alam = ("\n1.Jantur Mapan \n2.Danau Aco \n3.Kersik Luway")
+kutaibarat_kuliner = ("\n1.Keraton Resto & Art Gallery \n2.Grand Family Hotel")
+kutaibarat_keluarga = ("\n1.Taman Budaya Sendawar \n2.CIA Wisata")
+kutaibarat_belanja = ("\n1.Pasar Olah Bebaya")
+kutaibarat_sejarah = ("\n1.Lamin Tolan \n2.Pura Jagatnatha Sendawar Agung")
+kutaibarat_religi = ("\n1.Islamic Center")
+
+#Lokasi Wisata Kutai Kartanegara
+kutaikartanegara_alam = ("\n1.Hutan Pinus Samboja \n2.Bukit Bangkirai")
+kutaikartanegara_kuliner = ("\n1.Rumah Makan Minang Asli \n2.Rumah Makan Tepian Pandan")
+kutaikartanegara_keluarga = ("\n1.Ladang Buaya Tenggarong \n2.Ladaya")
+kutaikartanegara_sejarah = ("\n1.Museum Mulawarman \n2.Museum Kayu Tua Himba")
+kutaikartanegara_religi = ("\n1.Masjid Agung Sultan Sulaiman \n2.Masjid Jami' Adji Amir Hasanoeddin")
+
+#Lokasi Wisata Berau
+berau_alam = ("\n1.Labuan Cermin \n2.Pulau Derawan \n3.Pulau Kakaban")
+berau_kuliner = ("\n1.Kahyangan Seafood and Chinese Restaurant \n2.Rumah Makan Sari Ponti")
+berau_keluarga = ("\n1.Taman Wisata Bumi Bangun Lestari \n2.Taman Cendana \n3.Taman Sanggam")
+berau_belanja = ("\n1.Pasar Adji Dilayas")
+berau_sejarah = ("\n1.Keraton Sambaliung \n2.Keraton Gunung Tabur")
+berau_religi = ("\n1.Masjid Agung Baitul Hikmah \n2.Kelenteng Tri Dharma \n3.Makam Kubah Kuning")
